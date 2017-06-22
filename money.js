e1fb2253a7b02003c14b4704ad462a6a3717cd8e
JDude420 <joshmcneely04@gmail.com>
2017-06-22 18:50:55 -0500

    Update unknown
    Hopeful fix for client msg error

--- money.js
+++ money.js
@@ -1,18 +1,4 @@
 var updateLog = "v0.0b01";
-function updateFunc(){
-  try{
-    var out = new java.io.ByteArrayOutputStream();
-    var response = android.net.http.AndroidHttpClient.newInstance("Online()").execute(new org.apache.http.client.methods.HttpGet("https://raw.githubusercontent.com/JDude420/Money-Mod/master/update-checker-01.txt")).getEntity().writeTo(out);
-    out.close;
-    if(String(out.toString())==updateLog){
-      clientMessage("Your version: "+updateLog+"\nis the current\nupdate");
-      ModPE.showTipMessage("§a**----===----**");
-    }else{
-      clientMessage("Your version: "+updateLog+"\nis outdated. Get version"+String(out.toString())+"at\n§6https://raw.githubusercontent.com/JDude420/Money-Mod/master/money.js");
-      ModPE.showTipMessage("§4**----===----**");
-    }
-  }catch(e){clientMessage("Please connect to the internet | Reason: Update"); }
-}
 function newLevel(){
   updateFunc();
 }
@@ -27,4 +13,21 @@ function modTick(){
   if(getCarriedItem()==3240){
     ModPE.showTipMessage("§eYou have "+Player.getCarriedItemCount()+" coins left");
   }
+}
+function msg(txt){
+  clientMessage("[§4MoneyMod] §7"+txt);
+}
+function updateFunc(){
+  try{
+    var out = new java.io.ByteArrayOutputStream();
+    var response = android.net.http.AndroidHttpClient.newInstance("Online()").execute(new org.apache.http.client.methods.HttpGet("https://raw.githubusercontent.com/JDude420/Money-Mod/master/update-checker-01.txt")).getEntity().writeTo(out);
+    out.close;
+    if(String(out.toString())==updateLog){
+      msg("Your version: "+updateLog+"\nis the current\nupdate");
+      ModPE.showTipMessage("§a**----===----**");
+    }else{
+      msg("Your version: "+updateLog+"\nis outdated. Get version"+String(out.toString())+"at\n§6https://raw.githubusercontent.com/JDude420/Money-Mod/master/money.js");
+      ModPE.showTipMessage("§4**----===----**");
+    }
+  }catch(e){clientMessage("Please connect to the internet | Reason: Update"); }
 }
